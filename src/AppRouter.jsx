7ff9d9de8c2bcba8359f12c19bc8045f502ad53e
pietrorpedro <pietrorpedro@gmail.com>
2024-10-01 17:26:23 -0300

    config deploy

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/home/Home";
 import Search from "./pages/search/Search";
 
 const AppRouter = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Layout/>}>
                     <Route index element={<Home/>}/>
                     <Route path="/search" element={<Search/>}/>
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
